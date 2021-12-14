5a0ae87392743b40b79d4402299a6e86403aedae
defaultzer0 <84314463+defaultzer0@users.noreply.github.com>
2021-12-14 15:30:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+
+    buf = CircullarBuffer(4)
+
+    buf.push_front(1)
+    buf.push_back(2)
+    buf.push_front(3)
+    buf.push_front(4)
+    printBuf(buf)
+
+    save_buffer(buf)
+
+    new_buf = load_buffer()
+
+    printBuf(new_buf)
+
+if __name__ == "__main__":
+    main()
