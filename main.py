0fe1df81ae9c25e744f8d979032599d358037052
defaultzer0 <84314463+defaultzer0@users.noreply.github.com>
2021-12-14 15:51:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def main():
     
     print("push_front 1")
     buf.push_front(1)
-    printBuf(buf)
+    print(buf.buf)
     
     buf.push_back(2)
     print("push_back 2")
-    printBuf(buf)
+    print(buf.buf)
     
     print("push_back 3")
     buf.push_front(3)
-    printBuf(buf)
+    print(buf.buf)
     
     print("push_back 4")
     buf.push_front(4)
