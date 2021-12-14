f6896d1a4a62eb8ea8c9084d5f1d2698af6d08d5
defaultzer0 <84314463+defaultzer0@users.noreply.github.com>
2021-12-14 15:49:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ from CircularBuffer import CircularBuffer
 def main():
 
     buf = CircullarBuffer(4)
-
+    
+    print("push_front 1")
     buf.push_front(1)
+    printBuf(buf)
+    
     buf.push_back(2)
+    print("push_back 2")
+    printBuf(buf)
+    
+    print("push_back 3")
     buf.push_front(3)
+    printBuf(buf)
+    
+    print("push_back 4")
     buf.push_front(4)
     printBuf(buf)
-
+    
+    print("saving buf...")
     save_buffer(buf)
-
+    
+    print("loading buf...")
     new_buf = load_buffer()
-
+    
     printBuf(new_buf)
 
 if __name__ == "__main__":
